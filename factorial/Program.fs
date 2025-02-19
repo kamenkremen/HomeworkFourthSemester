dd32282ed084eda3728a0ade4803952e3bcd9d8f
kamenkremen <ivanz0610@gmail.com>
2025-02-19 19:01:21 +0000

    add task factorial

---
+++ factorial/Program.fs
@@ -0,0 +1,18 @@
+﻿let rec factorial n =
+    match n with
+    | n when n < 0 -> None
+    | 0 | 1 -> Some 1
+    | _ -> 
+    match factorial (n - 1) with
+        | None -> None
+        | Some number -> Some (number * n)
+
+printf "Enter number:"
+
+let input = System.Console.ReadLine()
+match System.Int32.TryParse(input) with 
+| true, number -> 
+    match factorial number with 
+    | Some result -> printfn "%d! = %d" number result
+    | None -> printfn "Number can`t be negative"
+| false, _ -> printfn "Incorrect input"
