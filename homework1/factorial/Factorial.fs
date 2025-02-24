b0fe52eaab647b81afa5f52ee8a463aa6488c57e
kamenkremen <ivanz0610@gmail.com>
2025-02-24 18:25:16 +0000

    remove interaction with user and change naming

---
+++ homework1/factorial/Factorial.fs
@@ -0,0 +1,8 @@
+﻿let rec factorial n =
+    match n with
+    | n when n < 0 -> None
+    | 0 | 1 -> Some 1
+    | _ -> 
+    match factorial (n - 1) with
+        | None -> None
+        | Some number -> Some (number * n)
