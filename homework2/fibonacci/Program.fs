92984fddecedb8a1f36fbf53d0630c826bfab45f
kamenkremen <ivanz0610@gmail.com>
2025-02-24 18:14:02 +0000

    add task fibonacci

---
+++ homework2/fibonacci/Program.fs
@@ -0,0 +1,9 @@
+﻿///A function to calculate the n-th Fibonacci number.
+let fibonacci n =
+    let rec fibonacciHelper = function
+        | n when n < 0 -> Error "value can`t be negative"
+        | 0 -> Ok 0
+        | 1 -> Ok 1
+        | n -> Ok(Result.defaultValue 0 (fibonacciHelper (n - 1)) + 
+            Result.defaultValue 0 (fibonacciHelper (n - 2)))
+    fibonacciHelper n
