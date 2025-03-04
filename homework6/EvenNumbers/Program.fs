ae3b27c004ec98849c9385ec8cdff49501131407
kamenkremen <ivanz0610@gmail.com>
2025-03-04 17:35:52 +0300

    Add task even numbers

---
+++ homework6/EvenNumbers/Program.fs
@@ -0,0 +1,18 @@
+﻿module EvenNumbers
+
+/// Function that counts even numbers in the given list and returns their amount, based on List.filter
+let CountEvenNumbersFilter list = 
+    list
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.length
+
+/// Function that counts even numbers in the given list and returns their amount, based on List.map
+let CountEvenNumbersMap list = 
+    list
+    |> List.map (fun x -> if x % 2 = 0 then 1 else 0)
+    |> List.sum
+
+/// Function that counts even numbers in the given list and returns their amount, based on List.fold
+let CountEvenNumbersFold list = 
+    list
+    |> List.fold (fun accumulator x -> if x % 2 = 0 then accumulator + 1 else accumulator) 0
---
+++ homework6/EvenNumbersTests/EvenNumbersTests.fs
@@ -0,0 +1,26 @@
+﻿module EvenNumbersTests
+
+open NUnit.Framework
+open EvenNumbers
+open FsCheck.NUnit
+open FsUnit
+
+let testCases () =
+    [ [ 1; 5; 2; 342; 123; 5 ], 2 
+      [ 2; 4; 6; 8 ], 4
+      [ ], 0
+      [ 1 ], 0
+      [ -2 ], 1
+      [ -1 ], 0 ]
+    |> List.map (fun (list, expected) -> TestCaseData(list, expected))
+
+[<Property>]
+let ``All functions return the same result`` (list: int list) =
+    let result1 = CountEvenNumbersMap list
+    let result2 = CountEvenNumbersFold list
+    let result3 = CountEvenNumbersFilter list
+    result1 = result2 && result2 = result3
+
+[<TestCaseSource(nameof(testCases))>]
+let ``CountEvenNumbersFilter should return correct amount of even numbers`` list expected =
+    list |> CountEvenNumbersFilter |> should equal expected
