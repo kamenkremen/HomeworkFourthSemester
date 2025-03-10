b6edb3633eacc582f65ce9be46f0d411709d6c7d
kamenkremen <ivanz0610@gmail.com>
2025-03-11 01:32:44 +0300

    add task parseTree

---
+++ homework8/ParseTree/ParseTree.fs
@@ -0,0 +1,18 @@
+﻿type ParseTree = 
+  | Number of int
+  | Add of ParseTree * ParseTree
+  | Subtract of ParseTree * ParseTree
+  | Multiply of ParseTree * ParseTree
+  | Divide of ParseTree * ParseTree
+
+/// the function that evaluates the value of a parse tree
+let rec evaluate tree = 
+    match tree with
+      | Number x -> x
+      | Add (left, right) -> evaluate left + evaluate right
+      | Subtract (left, right) -> evaluate left - evaluate right
+      | Multiply (left, right) -> evaluate left * evaluate right
+      | Divide (left, right) -> 
+        match evaluate right with 
+          | 0 -> failwith "Division by zero"
+          | _ -> evaluate left / evaluate right
---
+++ homework8/ParseTreeTests/ParseTreeTests.fs
@@ -0,0 +1,27 @@
+﻿
+module MapForTreesTests
+
+open NUnit.Framework
+open ParseTree
+open FsCheck.NUnit
+open FsUnit
+
+let testCases () =
+    [ (Add(Number(5), Number(3)), 8)
+      (Subtract(Number(10), Number(4)), 6)
+      (Multiply(Number(7), Number(2)), 14)
+      (Divide(Number(15), Number(3)), 5)
+      (Add(Multiply(Number(2), Number(5)), Subtract(Number(8), Number(3))), 15)
+      (Divide(Add(Number(10), Number(20)), Subtract(Number(8), Number(2))), 5)
+      (Multiply(Add(Subtract(Number(20), Number(5)), Divide(Number(15), Number(3))), Number(2)), 40)
+      (Divide(Multiply(Add(Number(3), Number(7)), Subtract(Number(5), Number(2))), Number(4)), 7) ]  
+    |> List.map (fun (tree, expected) -> TestCaseData(tree, expected))
+
+[<TestCaseSource(nameof(testCases))>]
+let ``evaluate should work correctly`` (tree: ParseTree) expected =
+    evaluate tree |> should equal expected
+
+[<Test>]
+let ``evaluate should throw exception when dividing by zero`` () =
+    (fun () -> evaluate (Divide(Number(10), Number(0))) |> ignore)
+    |> should throw typeof<System.Exception>
