888f74b9400e579e402b624fe86be2ff75534fc7
kamenkremen <ivanz0610@gmail.com>
2025-03-11 14:45:19 +0300

    fix the error with m < 0

--- homework5/PowerSeries/PowerSeries.fs
+++ homework5/PowerSeries/PowerSeries.fs
@@ -1,7 +1,7 @@
 ﻿/// Function that generates a series of powers of two from 2^n to 2^(n+m)
 let powerSeries n m =
     let rec helper currentValue count accumulator =
-        if count = 0 then
+        if count <= 0 then
             List.rev accumulator
         else
             helper (currentValue * 2) (count - 1) (currentValue :: accumulator)
