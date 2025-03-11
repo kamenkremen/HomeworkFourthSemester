c84457c4d3feb9c5499cf0f966c0a4a470467919
kamenkremen <ivanz0610@gmail.com>
2025-03-11 14:40:12 +0300

    add task primes

---
+++ homework9/Primes/Primes.fs
@@ -0,0 +1,9 @@
+﻿/// sequence of prime numbers
+let primes() = 
+    let rec sieve numbers = 
+        seq {
+            let x = Seq.head numbers
+            yield x
+            yield! sieve (numbers |> Seq.skip 1 |> Seq.filter (fun n -> n % x <> 0))
+        }
+    sieve (Seq.initInfinite (fun i -> i + 2))
---
+++ homework9/PrimesTests/PrimesTests.fs
@@ -0,0 +1,11 @@
+﻿module PrimesTests
+
+open Primes
+open NUnit.Framework
+open FsUnit
+
+
+[<Test>]
+let ``primes should produce first 25 prime numbers correctly`` () =
+    let expected = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]
+    primes() |> Seq.take 25 |> Seq.toList |> should equal expected
