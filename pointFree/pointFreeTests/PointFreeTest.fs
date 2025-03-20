da649f9c932f43d508517e29f4c4e60c976a27d2
kamenkremen <ivanz0610@gmail.com>
2025-03-20 22:53:23 +0300

    fix PointFreeTests

--- pointFree/pointFreeTests/PointFreeTest.fs
+++ pointFree/pointFreeTests/PointFreeTest.fs
@@ -1,11 +1,17 @@
 ﻿module pointFreeTests
 
 open PointFree
-open FsCheck.NUnit
+open NUnit.Framework
+open FsCheck
+open FsUnit
 
-[<Property>]
-let ``All functions return same result`` (x: int, l: int list) =
-    func1 x l = func2 x l 
-    && func2 x l = func3 x l 
-    && func3 x l = func4 x l
 
+[<Test>]
+let ``Functions should be equal``() = 
+    let equality1 x l = (func1 x l = func2 x l)
+    let equality2 x l = (func2 x l = func3 x l)
+    let equality3 x l = (func3 x l = func4 x l)
+    Check.Quick equality1
+    Check.Quick equality2
+    Check.Quick equality3
+    
