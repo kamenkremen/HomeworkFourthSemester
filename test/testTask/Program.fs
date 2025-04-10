f7f7ae826f79e9f6e5e4a30726a5922f87b502b7
kamenkremen <ivanz0610@gmail.com>
2025-04-10 16:25:10 +0300

    Add test tasks and one test

---
+++ test/testTask/Program.fs
@@ -0,0 +1,50 @@
+﻿module Test
+// First task
+let numbersSequence() = 
+    let ones = Seq.initInfinite (fun index -> 1 - 2 * (index % 2))
+    Seq.map2 (*) (Seq.initInfinite ((+) 1)) ones
+
+// Second task
+type Tree<'a> = 
+    | Leaf of 'a
+    | Node of Tree<'a> * Tree<'a>
+
+let rec filterForTrees tree predicate =
+    match tree with
+    | Leaf x -> 
+        match (predicate x) with
+        | true -> [x]
+        | false -> []
+    | Node (left, right) -> 
+        let leftValues = filterForTrees left predicate
+        let rightValues = filterForTrees right predicate
+        leftValues @ rightValues
+
+// Third task
+type PriorityQueue<'a>() =
+    /// Tuple with elements and priorities
+    let mutable elements: ('a * int) list = []
+
+    /// Enqueue priority queue
+    member this.Enqueue(item: 'a, priority: int) =
+        let rec insert list =
+            match list with
+            | [] -> [ (item, priority) ]
+            | (headValue, headPriority) :: tail ->
+                if priority > headPriority then
+                    (item, priority) :: list
+                else
+                    (headValue, headPriority) :: insert tail
+        elements <- insert elements
+
+    /// Dequeue priority queue. Throws System.InvalidOperationException if empty 
+    member this.Dequeue(): 'a =
+        match elements with
+        | [] -> raise (System.InvalidOperationException("Queue is empty"))
+        | (head, _) :: tail ->
+            elements <- tail
+            head
+
+    /// Returns whether priority queue is empty or not
+    member this.IsEmpty = List.isEmpty elements
+
---
+++ test/testTests/Tests.fs
@@ -0,0 +1,13 @@
+﻿module testTests
+open Test
+open NUnit.Framework
+open FsUnit
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let ``Test infinite sequence should produce first 10 numbers correctly `` () =
+    let expected = [1; -2; 3; -4; 5; -6; 7; -8; 9; -10]
+    numbersSequence() |> Seq.take 10 |> Seq.toList |> should equal expected
