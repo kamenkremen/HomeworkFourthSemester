d65599db5ca08fb8c3e4b65d2f495a55f015da8f
kamenkremen <ivanz0610@gmail.com>
2025-06-03 21:40:32 +0300

    add values to inner nodes

--- homework7/MapForTrees/MapForTrees.fs
+++ homework7/MapForTrees/MapForTrees.fs
@@ -1,9 +1,9 @@
 ﻿type Tree<'a> = 
     | Leaf of 'a
-    | Node of Tree<'a> * Tree<'a>
+    | Node of 'a * Tree<'a> * Tree<'a>
 
 /// Map for trees
 let rec mapForTrees tree func = 
     match tree with
     | Leaf x -> Leaf (func x)
-    | Node (left, right) -> Node (mapForTrees left func, mapForTrees right func)
+    | Node (value, left, right) -> Node (func value, mapForTrees left func, mapForTrees right func)
--- homework7/MapForTreesTests/MapForTreesTests.fs
+++ homework7/MapForTreesTests/MapForTreesTests.fs
@@ -6,9 +6,9 @@ open FsCheck.NUnit
 open FsUnit
 
 let testCases () =
-    [ Node(Leaf(1), Leaf(2)), (fun(x: int) -> x + 1), Node(Leaf(2), Leaf(3)) 
+    [ Node(2, Leaf(1), Leaf(3)), (fun(x: int) -> x + 1), Node(3, Leaf(2), Leaf(4)) 
       Leaf(2), (fun(x: int) -> x * 2), Leaf(4)
-      Node(Node(Leaf(1), Node(Leaf(2), Leaf(3))), Node(Leaf(4), Leaf(5))), (fun(x: int) -> x * 5), Node(Node(Leaf(5), Node(Leaf(10), Leaf(15))), Node(Leaf(20), Leaf(25)))
+      Node(6, Node(2, Leaf(1), Node(3, Leaf(4), Leaf(5))), Node(8, Leaf(7), Leaf(9))), (fun(x: int) -> x * 5), Node(30, Node(10, Leaf(5), Node(15, Leaf(20), Leaf(25))), Node(40, Leaf(35), Leaf(45)))
     ]
     |> List.map (fun (tree, mapFunction, expected) -> TestCaseData(tree, mapFunction, expected))
 
